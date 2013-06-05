e115a5c002ea885bdc8f298484511b4057cbecf1
Graham Bell <g.bell@jach.hawaii.edu>
2013-06-05 11:55:00 -1000

    Add geolocation module.

---
+++ lib/ukirt2caom2/geolocation.py
@@ -0,0 +1,19 @@
+from math import degrees
+
+import palpy
+
+from jcmt2caom2.geolocation import geolocation
+
+def ukirt_geolocation():
+    """Location of UKIRT as an (X, Y, Z) tuple.
+
+    Takes the coordinates of UKIRT from PAL and converts
+    them to X, Y, Z using jcmt2caom2.geolocation."""
+
+    ukirt = palpy.obs()['UKIRT']
+    return geolocation(degrees(ukirt['long']),
+                       degrees(ukirt['lat']),
+                       ukirt['height'])
+
+if __name__ == '__main__':
+    print(repr(ukirt_geolocation()))
