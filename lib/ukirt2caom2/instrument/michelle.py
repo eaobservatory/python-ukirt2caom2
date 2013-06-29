1cd0282e1378845f3d82ccc787241333a0e2f7e3
Graham Bell <g.bell@jach.hawaii.edu>
2013-06-28 14:23:58 -1000

    Started adding Michelle instrument modes.

--- lib/ukirt2caom2/instrument/michelle.py
+++ lib/ukirt2caom2/instrument/michelle.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from caom2 import Instrument
 from jcmt2caom2.raw import keywordvalue
 
@@ -6,11 +8,41 @@ from ukirt2caom2.instrument import instrument_classes
 from ukirt2caom2.instrument.ukirt import ObservationUKIRT
 from ukirt2caom2.util import clean_header
 
+logger = getLogger(__name__)
+
 class ObservationMichelle(ObservationUKIRT):
     def ingest_instrument(self, headers):
         instrument = Instrument('michelle')
 
         self.caom2.instrument = instrument
+
+        # Detector mode
+
+        if 'DETMODE' in headers[0]:
+            instrument.keywords.append(keywordvalue('mode',
+                                       headers[0]['DETMODE'].lower()))
+        elif 'DET_MODE' in headers[0]:
+            instrument.keywords.append(keywordvalue('mode',
+                                       headers[0]['DET_MODE'].lower()))
+
+        # Camera mode: imaging / spectroscopy / targetAcq
+        # also stored in the object as self.__camera
+
+        if 'INSTMODE' in headers[0]:
+            camera = headers[0]['INSTMODE'].lower()
+        elif 'CAMERA' in headers[0]:
+            camera = headers[0]['CAMERA'].lower()
+        else:
+            logger.warning('Instrument mode not found')
+            camera = None
+
+        if camera == '':
+            camera = None
+
+        if camera is not None:
+            instrument.keywords.append(keywordvalue('camera', camera))
+
+        self.__camera = camera
 
     def get_spectral_wcs(self, headers):
         return None
