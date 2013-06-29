6eae0a45961f049d2dca39adcc3cc892b67642ab
Graham Bell <g.bell@jach.hawaii.edu>
2013-06-28 17:25:08 -1000

    Add some instrument keywords for UIST.

--- lib/ukirt2caom2/instrument/uist.py
+++ lib/ukirt2caom2/instrument/uist.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from caom2 import Instrument
 from jcmt2caom2.raw import keywordvalue
 
@@ -6,11 +8,78 @@ from ukirt2caom2.instrument import instrument_classes
 from ukirt2caom2.instrument.ukirt import ObservationUKIRT
 from ukirt2caom2.util import clean_header
 
+logger = getLogger(__name__)
+
 class ObservationUIST(ObservationUKIRT):
     def ingest_instrument(self, headers):
         instrument = Instrument('uist')
 
         self.caom2.instrument = instrument
+
+        # Camera mode: ifu / imaging / spectroscopy
+        camera = headers[0]['INSTMODE']
+
+        if camera == '':
+            camera = None
+            logger.warning('No camera mode specified')
+
+        self.__camera = camera
+
+        if camera is not None:
+            instrument.keywords.append(keywordvalue('camera', camera))
+
+        # Detector mode
+        mode = headers[0]['DET_MODE'].lower()
+        instrument.keywords.append(keywordvalue('mode', mode))
+
+        # Readout mode
+        readout = headers[0].get('READOUT', None)
+
+        if readout is not None:
+            instrument.keywords.append(keywordvalue('readout', readout.lower()))
+
+        # Polarizer?
+        pol = headers[0]['POLARISE']
+        if pol is True:
+            instrument.keywords.append(keywordvalue('pol', 'true'))
+        elif pol is False:
+            instrument.keywords.append(keywordvalue('pol', 'false'))
+        else:
+            pol = None
+
+        self.__pol = pol
+
+        # Lens
+        lens = headers[0]['CAMLENS']
+        if lens != '':
+            instrument.keywords.append(keywordvalue('lens', lens.lower()))
+
+        # Filter
+        filter = headers[0]['FILTER']
+        if filter == '':
+            filter = None
+
+        self.__filter = filter
+
+        # Grism
+        grism = headers[0]['GRISM']
+        if grism == '':
+            grism = None
+
+        if grism is not None:
+            instrument.keywords.append(keywordvalue('grism', grism))
+
+        self.__grism = grism
+
+        # Slit
+        slit = headers[0]['SLITNAME']
+        if slit == '':
+            slit = None
+
+        if slit is not None:
+            instrument.keywords.append(keywordvalue('slit', slit))
+
+        self.__slit = slit
 
     def get_spectral_wcs(self, headers):
         return None
