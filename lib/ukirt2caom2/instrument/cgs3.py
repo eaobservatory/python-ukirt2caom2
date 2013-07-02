8087315dbb10017842e093fe34be4ac697822d71
Graham Bell <g.bell@jach.hawaii.edu>
2013-07-01 17:53:37 -1000

    Add custom intent_type method for CGS3.

--- lib/ukirt2caom2/instrument/cgs3.py
+++ lib/ukirt2caom2/instrument/cgs3.py
@@ -45,10 +45,46 @@ class ObservationCGS3(ObservationUKIRT):
 
         self.__grating = grating
 
+        # Chopper
+
+        instrument.keywords.append(keywordvalue('chopper', self.__chopper))
+
     def ingest_type_intent(self, headers):
-        # CGS3 will need a custom version of this method as it
-        # does not have an OBSTYPE header.
-        pass
+        # CGS3 needs a custom version of this method as it
+        # does not have an OBSTYPE header.  It looks like
+        # we need to guess from the object name, but all
+        # entries with chopper == 'sector' are probably calibrations.
+
+        self.__chopper = headers[0]['C3CHOPPR'].lower()
+
+        krypton = ('arc', 'kr', 'krypton', 'kr_lamp', 'lamp')
+
+        object = headers[0]['OBJECT']
+
+        if object.lower() in krypton:
+            type = 'arc'
+
+        elif object.startswith('SKY for'):
+            type = 'sky'
+
+            if self.__chopper == 'secondary':
+                logger.warning('SKY in non-secondary chopping')
+
+        else:
+            type = 'object'
+
+            if self.__chopper != 'secondary':
+                logger.warning('Unknown object ' + object + ' in non-secondary chopping')
+
+        # Add the data to the CAOM-2 object
+
+        if type == 'object':
+            self.caom2.intent = ObservationIntentType.SCIENCE
+
+        else:
+            self.caom2.intent = ObservationIntentType.CALIBRATION
+
+        self.obstype = type
 
     def get_spectral_wcs(self, headers):
         filter = headers[0]['C3FILT']
