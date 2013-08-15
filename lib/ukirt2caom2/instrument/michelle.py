53c1001ea88da7dad430f2d00e9781e61b7df3e9
Graham Bell <g.bell@jach.hawaii.edu>
2013-08-14 14:56:21 -1000

    Parse Michelle filter names.
    Michelle filters appear to be systematically names,
    so we can parse them to extract the central frequency
    and bandwidth.

--- lib/ukirt2caom2/instrument/michelle.py
+++ lib/ukirt2caom2/instrument/michelle.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+import re
 
 from caom2 import Instrument
 from jcmt2caom2.raw import keywordvalue
@@ -9,6 +10,28 @@ from ukirt2caom2.instrument.ukirt import ObservationUKIRT
 from ukirt2caom2.util import clean_header
 
 logger = getLogger(__name__)
+
+def parse_filter(value):
+    """Attempt to interpret name of Michelle filter."""
+
+    m = re.match('^([EIF])(P?)(\d+)B(\d+)$', value)
+
+    if m is None:
+        return (None, None)
+
+    (prefix, pol, centre, bandpass) = m.groups()
+
+    name = 'F{}B{}'.format(centre, bandpass)
+
+    pol = pol == 'P'
+
+    centre = float(centre) / 10.0 # um
+    bandpass = float(bandpass) / 100.0 # %
+
+    cut_on = centre - 0.5 * centre * bandpass
+    cut_off = centre + 0.5 * centre * bandpass
+
+    return ((cut_on, cut_off, name), pol)
 
 class ObservationMichelle(ObservationUKIRT):
     def ingest_instrument(self, headers):
@@ -46,12 +69,13 @@ class ObservationMichelle(ObservationUKIRT):
 
         # Filter Wheel
 
-        filter = headers[0]['FILTER']
-
-        if filter == '':
-            filter = None
+        (filter, pol) = parse_filter(headers[0]['FILTER'])
 
         self.__filter = filter
+        self.__pol = pol
+
+        instrument.keywords.append(keywordvalue('pol',
+                                   'true' if pol else 'false'))
 
         # Grating Drum
 
