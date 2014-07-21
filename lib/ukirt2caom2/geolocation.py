003e8b0c5729ae51bd85fc872c0d08ffa7ceee9e
Graham Bell <g.bell@jach.hawaii.edu>
2014-07-21 09:38:50 -1000

    Swap sign of latitude returned by palpy
    tools4caom2 appears to use a different sign convention.  Also
    added some assert statements to check that the final values
    have the signs we expect.

--- lib/ukirt2caom2/geolocation.py
+++ lib/ukirt2caom2/geolocation.py
@@ -8,12 +8,24 @@ def ukirt_geolocation():
     """Location of UKIRT as an (X, Y, Z) tuple.
 
     Takes the coordinates of UKIRT from PAL and converts
-    them to X, Y, Z using jcmt2caom2.geolocation."""
+    them to X, Y, Z using jcmt2caom2.geolocation.
+
+    Note: this routine swaps the sign of the longitude
+    returned by PAL because PAL uses a west-positive
+    convention whereas tools4caom2 appears to use
+    east-positive."""
 
     ukirt = palpy.obs()['UKIRT']
-    return geolocation(degrees(ukirt['long']),
+    geo = geolocation(- degrees(ukirt['long']),
                        degrees(ukirt['lat']),
                        ukirt['height'])
+
+    # Check sign convention!
+    assert(geo[0] < 0.0)
+    assert(geo[1] < 0.0)
+    assert(geo[2] > 0.0)
+
+    return geo
 
 if __name__ == '__main__':
     print(repr(ukirt_geolocation()))
